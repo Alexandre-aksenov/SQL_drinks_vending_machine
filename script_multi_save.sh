a5e715b7621764e9660813567ad99c77e4cb1170
Alexandre Aksenov <alexander1aksenov@gmail.com>
2026-01-17 21:10:12 +0100

    Bash: refactored the test for a valid query

--- script_multi_save.sh
+++ script_multi_save.sh
@@ -5,9 +5,18 @@ SQL_FILE="distributeur_query.sql"
 mkdir -p results
 counter=1
 
+is_not_query () {
+  # ask whether the text does not begin with the word SELECT.
+  # Returns 0 if it is a SELECT, 1 otherwise.
+  local cand_query=$1
+  [ "${cand_query#SELECT}" = "$cand_query" ]; echo "$?"
+}
+
 while read -rd ';' sql
 do
-    if [ "${sql#SELECT}" = "$sql" ] # ask whether the text does not begin with the word SELECT.
+    #if [ "${sql#SELECT}" = "$sql" ] # ask whether the text does not begin with the word SELECT.
+    #->
+    if [[ $(is_not_query "$sql") -eq 0 ]]
     then
         echo "Not a SELECT!"
     else
